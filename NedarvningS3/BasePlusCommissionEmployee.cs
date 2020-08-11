26a8c7d06d4fbdc84616149f4019211f35b37f57
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-11 11:06:55 +0200

    cleaned up code

---
+++ NedarvningS3/BasePlusCommissionEmployee.cs
@@ -0,0 +1,27 @@
+﻿
+namespace NedarvningS3
+{
+    class BasePlusCommissionEmployee: CommissionEmployee
+    {
+        protected double baseSalary;
+
+        public BasePlusCommissionEmployee(string firstName, string lastName, string socialSecurityNumber, double grossSales, double commissionRate,
+            double baseSalary) : base(firstName, lastName, socialSecurityNumber, grossSales, commissionRate)
+        {
+            BaseSalary = baseSalary;
+        }
+
+        public virtual double BaseSalary
+        {
+            get
+            {
+                return baseSalary;
+            }
+
+            set
+            {
+                baseSalary = value;
+            }
+        }
+    }
+}
